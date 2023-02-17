b6883716021d2d1bbc869969c04346dc31295641
Nathaniel <tshego.tt.morare@gmail.com>
2023-02-17 13:15:31 -0800

    dont print others

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry level function
+ *
+ * Return: return zero for now
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		if (i != 'q' && i != 'e')
+			putchar(i);
+	putchar('\n');
+	return (0);
+}
