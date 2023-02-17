5f3c96805f474bc8dd2668557fd6f24de7803fc5
Nathaniel <tshego.tt.morare@gmail.com>
2023-02-17 13:46:17 -0800

    reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - entry level function
+ *
+ * Return: return zero
+ */
+
+int main(void)
+{
+	char i;
+
+	for (i = 'z'; i <= 'a'; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
