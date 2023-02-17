3e64092b80d6c09e24af64a03702f51972b0e666
Nathaniel <tshego.tt.morare@gmail.com>
2023-02-17 12:56:02 -0800

    alphabet game

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - the entry function to this program
+ *
+ * Return: return nothing in this case
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 'a' ; i <= 'z' ; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
