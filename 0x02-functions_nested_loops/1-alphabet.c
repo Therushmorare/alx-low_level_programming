189fbd0cfc014b67f0ba4742256869e78ab3158e
Nathaniel <tshego.tt.morare@gmail.com>
2023-02-21 14:05:30 -0800

    alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphabet -print_alphabet  print alphabet in lowercase'
+ * Return: always return 0
+ */
+
+void print_alphabet(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+	_putchar('\n');
+}
