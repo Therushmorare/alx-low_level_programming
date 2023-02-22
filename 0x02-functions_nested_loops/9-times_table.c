83af949cc16820e8fd4cf28a05ae3bf800e810d9
Nathaniel <tshego.tt.morare@gmail.com>
2023-02-22 15:54:17 -0800

    nine times tables

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,1 +1,23 @@
+#include "main.h"
+#include <stdio.h>
 
+/**
+ *times_table - calculaate 9 times tables
+ *
+ *Return: always zero
+ */
+
+void times_table(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			printf("%d, ", i * j);
+		}
+		printf("%d\n", i * 9);
+	}
+}
