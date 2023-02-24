233aa900d3686b0a6216c7102cfada647521f37e
Nathaniel <tshego.tt.morare@gmail.com>
2023-02-23 16:51:02 -0800

    numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *print_numbers - print 0 to 9
+ *
+ *Return: always 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar('0' + i);
+	}
+	_putchar('\n');
+}
