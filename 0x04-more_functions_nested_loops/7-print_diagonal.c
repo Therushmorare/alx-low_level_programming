3c39c65c2bceac49488980c45ca42dbf9fc08feb
Nathaniel <tshego.tt.morare@gmail.com>
2023-02-24 07:10:01 -0800

    diagonally parallel universe

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *print_diagonal - print \ diagonally on screen
+ *@n: attribute to be used
+ *Return: always zero
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n > 0)
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
