be5ae2f4155f3fd6eff9b2f68386f026d920d8e5
Nathaniel <tshego.tt.morare@gmail.com>
2023-02-27 08:54:39 -0800

    swaping pointer values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *swap_int - function to swap a and b
+ *@a: pointer var
+ *@b: pointer var
+ *Return: always zero
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
