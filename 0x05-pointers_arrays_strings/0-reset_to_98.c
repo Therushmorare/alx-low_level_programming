ba84a5babc3841c747954bd0a3525fccfbde5856
Nathaniel <tshego.tt.morare@gmail.com>
2023-02-27 08:47:01 -0800

    pointers

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *reset_to_98 - function that takes up pointer
+ *@n: the attribute to used
+ *
+ *Return: always zero
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
