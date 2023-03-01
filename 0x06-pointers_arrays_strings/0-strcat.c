4785bd27ea5f8e64b631a122b585f8789ec7a67b
Nathaniel <tshego.tt.morare@gmail.com>
2023-03-01 08:25:27 -0800

    string copy

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *_strcat - function to concatinate strings
+ *@src: starting string
+ *@dest: last string
+ *Return: return concat value
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+more poointers and arrays
