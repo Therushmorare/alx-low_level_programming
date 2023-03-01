62b0bf05eabca5be7570fc6b19f5df1597d51335
Nathaniel <tshego.tt.morare@gmail.com>
2023-03-01 13:23:21 -0800

    first letter capitals

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+#include <ctype.h>
+
+/**
+ *cap_string - print first letters as caps
+ *@str: attribute to be used
+ *
+ *Return: return string
+ */
+
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i == 0 || str[i - 1] == ' ' || str[i - 1] == '\t' || str[i - 1] == '\n' ||
+			str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '.' ||
+			str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' ||
+			str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' ||
+			str[i - 1] == '}')
+				str[i] = toupper(str[i]);
+	}
+	return (str);
+}
