3f016c276e781591bfca5588e1e1095eb41b576d
Nathaniel <tshego.tt.morare@gmail.com>
2023-03-01 09:20:36 -0800

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ *_strncpy -  function that copies two strings
+ *@dest: pointer that src will be copied to
+ *@src: pointer that will be used to transfere data to dest
+ *@n: size of string
+ *Return: return dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; i < n && src[i] != '\0'; i++)
+		dest[i] = src[i];
+	for (; i < n ; i++)
+		dest[i] = '\0';
+
+	return (dest);
+}
