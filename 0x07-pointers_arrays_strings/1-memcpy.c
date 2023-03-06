44b4d5396140f438813499af0ea6398aa485cb69
Nathaniel <tshego.tt.morare@gmail.com>
2023-03-06 12:43:47 -0800

    memory copy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *memcpy - copy memory to another var
+ *_memcpy - copy memory to another var
  *@dest: the destination where memory is to be copied to
  *@src: source of the copied memory
  *@n: the size of the buffer
@@ -10,7 +10,7 @@
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned i;
+	unsigned int i;
 
 	for (i = 0; i < n ; i++)
 	{
