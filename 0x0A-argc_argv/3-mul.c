6d572ac0b396abf63147d0f829499e5f0b359800
Nathaniel <tshego.tt.morare@gmail.com>
2023-03-10 12:49:03 -0800

    multiply

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *main - main function
+ *@argc: counter
+ *@argv: vector array
+ *
+ *Return: result
+ */
+
+int main(int argc, char **argv)
+{
+	int i;
+	int k;
+	int result;
+
+	if (argc)
+	{
+		if (argc != 3)
+			printf("Error\n");
+
+		i = atoi(argv[1]);
+		k = atoi(argv[2]);
+
+		result = i * k;
+		printf("%d\n", result);
+	}
+	return (result);
+}
