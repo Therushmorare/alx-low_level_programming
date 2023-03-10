0f5f705264f45a1b5a3be621117ae074dbc73378
Nathaniel <tshego.tt.morare@gmail.com>
2023-03-10 12:25:00 -0800

    print stuff

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -12,17 +12,9 @@ int main(int argc, char *argv[])
 {
 	int count;
 
-	if (argc > 0)
+	for (count = 0; count < argc; count++)
 	{
-		for (count = 0; count < argc; count++)
-		{
-			printf("argv[%d] = %s\n", count, argv[count]);
-		}
+		printf("%s\n", argv[count]);
 	}
-	else
-	{
-		printf("The command had no other arguments.\n");
-	}
-
 	return (0);
 }
