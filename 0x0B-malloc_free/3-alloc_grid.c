37f2b01544ec07cc699ff45d748dab9887062a0e
Nathaniel <tshego.tt.morare@gmail.com>
2023-03-13 14:29:54 -0700

    grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,7 +25,7 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 
-	for (i = 0; i <= height; i++)
+	for (i = 0; i < height; i++)
 	{
 		arr[i] = malloc(width * sizeof(int));
 		if (arr[i] == NULL)
@@ -38,7 +38,7 @@ int **alloc_grid(int width, int height)
 			return (NULL);
 		}
 
-		for (j = 0; j <= width; j++)
+		for (j = 0; j < width; j++)
 		{
 			arr[i][j] = 0;
 		}
