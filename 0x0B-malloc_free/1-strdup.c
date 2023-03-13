fcf789e2e7a7891025f58688954d7678ce1b8e0d
Nathaniel <tshego.tt.morare@gmail.com>
2023-03-13 11:16:15 -0700

    copy to new string

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *_strdup - copies string print to new string
+ *@str: string to be used
+ *
+ *Return: new str
+ */
+
+char *_strdup(char *str)
+{
+	int i;
+	int j;
+	char *new_str;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (j = 0; j <= *str; j++)
+	{
+	}
+	j += 1;
+
+	new_str = malloc(j * sizeof(char));
+
+	for (i = 0; i < j; i++)
+		new_str[i] = str[i];
+
+	return (new_str);
+}
