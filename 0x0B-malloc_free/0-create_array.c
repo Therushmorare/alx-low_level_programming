44d28b28439a0f19c38f04421ba8ba39d8f2b6b2
Nathaniel <tshego.tt.morare@gmail.com>
2023-03-13 10:52:16 -0700

    dynamic array

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,20 +16,14 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	if (size != 0)
-	{
-		ar = malloc(size * sizeof(char));
 
-		for (i = 0; i < size; i++)
-		{
-			ar[i] = c;
-		}
-	}
-	else
-	{
+	ar = malloc(size * sizeof(char));
+
+	if (ar == NULL)
 		return (NULL);
-	}
+
+	for (i = 0; i < size; i++)
+		ar[i] = c;
 
 	return (ar);
-	free(ar);
 }
