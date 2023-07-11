7546be0fb426d83a88f76cbac7266a77c154ce98
Tshegofatso Morare <tshego.tt.morare@gmail.com>
2023-07-10 18:29:58 -0700

    create update

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,28 +1,50 @@
 #include "main.h"
 
 /**
- *create_file - create file
- *@filename: name of file
- *@text_content: content of the file
+ * _strlen - compute the length of a NULL-terminated string
+ * @str: the string to measure
  *
- *Return: 1 on success
+ * Return: the length of str, or -1 if str is NULL
  */
+ssize_t _strlen(const char *str)
+{
+	ssize_t len = 0;
 
+	if (!str)
+		return (-1);
+
+	while (*str++)
+		++len;
+
+	return (len);
+}
+
+/**
+ * create_file - create a file
+ * @filename: the name of the file to create
+ * @text_content: the data to write to filename
+ *
+ * Return: Upon success, return 1. Otherwise, return -1.
+ */
 int create_file(const char *filename, char *text_content)
 {
-	FILE *file_name;
+	ssize_t b_written = 0;
+	int file_dir;
 
-	file_name = fopen(filename, "w+");
-
-	if (filename == NULL || file_name)
+	if (!filename)
 		return (-1);
 
-	if (text_content == NULL)
+	file_dir = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+
+	if (file_dir < 0)
 		return (-1);
 
-	fputs(text_content, file_name);
+	if (text_content)
+		b_written = write(file_dir, text_content, _strlen(text_content));
 
-	fclose(file_name);
+	close(file_dir);
 
+	if (b_written < 0)
+		return (-1);
 	return (1);
 }
