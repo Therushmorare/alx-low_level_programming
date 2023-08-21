f58bd7c56c204f9f7a5df7fe521754b45e842a59
tshegofatso <tshego.tt.morare@gmail.com>
2023-08-21 11:06:28 -0700

    shell script

---
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Compile all .c files in the current directory into object files
+gcc -Wall -Werror -Wextra -pedantic -c *.c
+
+# Create the dynamic library liball.so from the compiled object files
+gcc -shared -o liball.so *.o
+
+# Clean up by removing the compiled object files
+rm -f *.o
